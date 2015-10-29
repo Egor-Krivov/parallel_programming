26351bfff4d1b61f9a412c9298d3cfe7e886537a
Egor Krivov <egor.krivov@frtk.ru>
2015-10-29 22:42:09 +0300

    time measurements were added, bug with the lack of \n in cons program was fixed

--- mpi_lab/mpi_lab.c
+++ mpi_lab/mpi_lab.c
@@ -3,19 +3,27 @@
 #include <assert.h>
 #include <math.h>
 
-#define ISIZE 1000
-#define JSIZE 10000
-#define MUL 0.0000001
+#define ISIZE 10000
+#define JSIZE 1000
+#define MUL 0.000001
 
 int main(int argc, char* argv[]) {
     int i, j;
+    int results = 1;
+    if (argc > 1)
+        results = 0;
     FILE *ff = fopen("results.txt", "w");
     double val;
-    for (i = 0; i < ISIZE; i++)
+    for (i = 0; i < ISIZE; i++) {
         for (j = 0; j < JSIZE; j++) {
             val = 10 * i + j;
-            fprintf(ff, "%lf ", sin(MUL * val));
+            val = sin(MUL * val);
+	    if (results)
+                fprintf(ff, "%lf ", val);
         }
+        if (results)
+            fprintf(ff, "\n");
+    }
     fclose(ff);
     return 0;
 }
--- mpi_lab/mpi_lab_parallel.c
+++ mpi_lab/mpi_lab_parallel.c
@@ -4,13 +4,16 @@
 #include <assert.h>
 #include <math.h>
 
-#define ISIZE 1000
-#define JSIZE 10000
-#define MUL 0.0000001
+#define ISIZE 10000
+#define JSIZE 1000
+#define MUL 0.000001
 
 int main(int argc, char* argv[]) {
 
     int rank, size;
+    int results = 1;
+    if (argc > 1)
+        results = 0;
     MPI_Status status;
     MPI_Request request;
     MPI_Init(&argc,&argv);
@@ -26,33 +29,38 @@ int main(int argc, char* argv[]) {
     double *data;
     double val;
     data = (double *) malloc(sizeof(double) * JSIZE * rows);
+    double t1 = MPI_Wtime();
     for (i = 0; i < rows; i++)
         for (j = 0; j < JSIZE; j++) {
             val = 10 * (i + first_number) + j;
             val = sin(MUL * val);
             data[i * JSIZE + j] = val;
         }
+    double t2 = MPI_Wtime();
+    if (rank == 0)
+        printf("%lf\n", t2 - t1);
+    if (results) {
+        FILE *ff;
     
-    FILE *ff;
-    
-    if (rank != 0) {
-        int source = rank - 1;
-        MPI_Recv(0, 0, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
-        ff = fopen("results.txt", "a");
-    }
-    else {
-        ff = fopen("results.txt", "w");
-    }
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < JSIZE; j++) {
-            fprintf(ff, "%lf ", data[i * JSIZE + j]);
+        if (rank != 0) {
+            int source = rank - 1;
+            MPI_Recv(0, 0, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+            ff = fopen("results.txt", "a");
         }
-        fprintf(ff, "\n");
-    }
-    fclose(ff);
-    if (rank != size - 1) {
-        int dest = rank + 1;
-        MPI_Send(0, 0, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        else {
+            ff = fopen("results.txt", "w");
+        }
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < JSIZE; j++) {
+                fprintf(ff, "%lf ", data[i * JSIZE + j]);
+            }
+            fprintf(ff, "\n");
+        }
+        fclose(ff);
+        if (rank != size - 1) {
+            int dest = rank + 1;
+            MPI_Send(0, 0, MPI_INT, dest, 0, MPI_COMM_WORLD);
+        } 
     }
     free(data);
     MPI_Finalize();
